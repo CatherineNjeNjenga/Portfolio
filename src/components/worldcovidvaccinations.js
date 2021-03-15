2dd7cec926380885cf5185192c342db1f4e55ff6
Bryan Yi <bryanyi1206@gmail.com>
2021-03-15 08:28:50 -0700

    designing project details page

--- src/components/worldcovidvaccinations.js
+++ src/components/worldcovidvaccinations.js
@@ -6,7 +6,35 @@ const worldcovidvaccinationsScreen = () => {
       <div className="title">
         <h1>World COVID19 Vaccinations</h1>
       </div>
-      <div className="key__features"></div>
+      <div className="key__features">
+        <div className="features__list">
+          <h2>Key Features</h2>
+          <ul>
+            <li>
+              4000+ records collected from{" "}
+              <span className="feat_emph">Kaggle</span> and stored in{" "}
+              <span className="feat_emph">
+                AWS Relational Database Service (RDS)
+              </span>
+              .
+            </li>
+            <li>
+              Class based views using <span className="feat_emph">Django</span>
+            </li>
+            <li>
+              Implemented Django to build backend{" "}
+              <span className="feat_emph">RESTful APIs</span> endpoints to serve
+              JSON data constructed from raw{" "}
+              <span className="feat_emph">MySQL</span>queries
+            </li>
+            <li>
+              Customized <span className="feat_emph">Chart.js</span> to display
+              data to enhance the user experience and user interface.
+            </li>
+          </ul>
+        </div>
+        <div className="image"></div>
+      </div>
 
       <div className="project__details">
         <div className="api__design"></div>
